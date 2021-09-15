ba092d2bcc99d6426bb20de4323e0f7245083b18
Łukasz Lassota <lukaszlassota@gmail.com>
2021-09-16 01:18:18 +0200

    Add article data structure interface.

---
+++ src/models/article.tsx
@@ -0,0 +1,10 @@
+export interface Article {
+  url: string;
+  title: string;
+  description: string;
+  sourceTitle: string;
+  domain: string;
+  favicon: string;
+  image: string;
+  imageAlt: string;
+}
