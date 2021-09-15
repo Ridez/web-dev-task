dd30dfd574bb6d5dd512241bee64a2ce21deb6c2
Łukasz Lassota <lukaszlassota@gmail.com>
2021-09-16 01:21:05 +0200

    Store articles data in array.

---
+++ src/data/articles.tsx
@@ -0,0 +1,106 @@
+import { Article } from '../models/article';
+
+export const TodaysPicks: Article[] = [
+  {
+    url: 'https://www.example.com/',
+    title: '6 ways to achieve ‘wow’ customer service and create lifelong customers',
+    description:
+      '7h ago · All humans, including the humans we call "customers," tend to remember what happens',
+    sourceTitle: 'Forbes',
+    domain: 'https://www.forbes.com',
+    favicon: '/assets/images/Image 16-1@2x.png',
+    image: '/assets/images/Image 7@2x.png',
+    imageAlt: 'Desc of image',
+  },
+  {
+    url: 'https://www.example.com/',
+    title: 'How do organizations spread and use analytic knowledge?',
+    description:
+      '7h ago · There are many ways to achieve great analytic cultures, but all great analytic cultures share some common elements. The best',
+    sourceTitle: 'BBC Future',
+    domain: 'https://www.bbc.com/future',
+    favicon: '/assets/images/WLwRt2Wv_400x400@2x.png',
+    image: '/assets/images/photo-1551288049-bebda4e38f71@2x.png',
+    imageAlt: 'Desc of image',
+  },
+  {
+    url: 'https://www.example.com/',
+    title: 'The benefits of knowledge management for your service desk',
+    description:
+      '7h ago · Getting knowledge management right has never been easy. You know that in the long run, recording the knowledge of your service desk',
+    sourceTitle: 'Zendesk',
+    domain: 'https://www.zendesk.com/',
+    favicon: '/assets/images/download@2x.png',
+    image: '/assets/images/18-km-benefits@2x.png',
+    imageAlt: 'Desc of image',
+  },
+  {
+    url: 'https://www.example.com/',
+    title: '6 ways to achieve ‘wow’ customer service and create lifelong customers',
+    description:
+      '7h ago · All humans, including the humans we call "customers," tend to remember what happens',
+    sourceTitle: 'Forbes',
+    domain: 'https://www.forbes.com',
+    favicon: '/assets/images/Image 16-1@2x.png',
+    image: '/assets/images/Image 7@2x.png',
+    imageAlt: 'Desc of image',
+  },
+  {
+    url: 'https://www.example.com/',
+    title: 'How do organizations spread and use analytic knowledge?',
+    description:
+      '7h ago · There are many ways to achieve great analytic cultures, but all great analytic cultures share some common elements. The best',
+    sourceTitle: 'BBC Future',
+    domain: 'https://www.bbc.com/future',
+    favicon: '/assets/images/WLwRt2Wv_400x400@2x.png',
+    image: '/assets/images/photo-1551288049-bebda4e38f71@2x.png',
+    imageAlt: 'Desc of image',
+  },
+  {
+    url: 'https://www.example.com/',
+    title: 'The benefits of knowledge management for your service desk',
+    description:
+      '7h ago · Getting knowledge management right has never been easy. You know that in the long run, recording the knowledge of your service desk',
+    sourceTitle: 'Zendesk',
+    domain: 'https://www.zendesk.com/',
+    favicon: '/assets/images/download@2x.png',
+    image: '/assets/images/18-km-benefits@2x.png',
+    imageAlt: 'Desc of image',
+  },
+];
+
+export const EmployeeBranding: Article[] = [
+  {
+    url: 'https://www.example.com/',
+    title: '6 ways to achieve ‘wow’ customer service and create lifelong customers',
+    description:
+      '7h ago · All humans, including the humans we call "customers," tend to remember what happens',
+    sourceTitle: 'Forbes',
+    domain: 'https://www.forbes.com',
+    favicon: '/assets/images/Image 16-1@2x.png',
+    image: '/assets/images/Image 7@2x.png',
+    imageAlt: 'Desc of image',
+  },
+  {
+    url: 'https://www.example.com/',
+    title: 'How do organizations spread and use analytic knowledge?',
+    description:
+      '7h ago · There are many ways to achieve great analytic cultures, but all great analytic cultures share some common elements. The best',
+    sourceTitle: 'BBC Future',
+    domain: 'https://www.bbc.com/future',
+    favicon: '/assets/images/WLwRt2Wv_400x400@2x.png',
+    image: '/assets/images/photo-1551288049-bebda4e38f71@2x.png',
+    imageAlt: 'Desc of image',
+  },
+  {
+    url: 'https://www.example.com/',
+    title: 'The benefits of knowledge management for your service desk',
+    description:
+      '7h ago · Getting knowledge management right has never been easy. You know that in the long run, recording the knowledge of your service desk',
+    sourceTitle: 'Zendesk',
+    domain: 'https://www.zendesk.com/',
+    favicon: '/assets/images/download@2x.png',
+    image: '/assets/images/18-km-benefits@2x.png',
+    imageAlt: 'Desc of image',
+  },
+];
