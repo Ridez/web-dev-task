4fc4cca751612f0c2f76111f8e93195fdeaa89c3
Łukasz Lassota <lukaszlassota@gmail.com>
2021-09-16 01:23:15 +0200

    Add articles page.

---
+++ src/pages/articles.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ArticlesList } from '../components/ArticlesList';
+
+export const Articles = () => {
+  return (
+    <>
+      <div className='container'>
+        <ArticlesList></ArticlesList>
+      </div>
+    </>
+  );
+};
